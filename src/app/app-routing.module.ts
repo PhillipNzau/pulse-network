996d7c13b6c3535aa56b5ca7b879bc77175fcf66
phillipnzau <nzauonline@gmail.com>
2023-07-06 20:32:21 +0300

    Routes set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', loadComponent:() => import('./pages/home/home.component').then(mod=> mod.HomeComponent)},
+  {path:'about', loadChildren:() => import('./pages/about/about.routes').then(mod=> mod.ABOUT_ROUTES)},
+  {path:'contact', loadComponent:() => import('./pages/contact/contact.component').then(mod=> mod.ContactComponent)},
+  {path:'demo', loadComponent:() => import('./pages/request-demo/request-demo.component').then(mod=> mod.RequestDemoComponent)},
+  {path:'solutions', loadComponent:() => import('./pages/solutions/solutions.component').then(mod=> mod.SolutionsComponent)},
+  {path:'shop', loadComponent:() => import('./pages/shop/shop.component').then(mod=> mod.ShopComponent)},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
