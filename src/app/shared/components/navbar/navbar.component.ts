6a76b06051f989fabdc8f9352e5bc815655860ec
phillipnzau <nzauonline@gmail.com>
2023-07-07 09:37:43 +0300

    navbar complete

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  homeTop = '../../../../assets/imgs/home-top.png'
+  aboutCsTop = '../../../../assets/imgs/about-cs-top.png'
+  aboutIrTop = '../../../../assets/imgs/about-ir-top.png'
+  aboutTop = '../../../../assets/imgs/about-top.png'
+  shopTop = '../../../../assets/imgs/shop-top.png'
+
+  topBar:string = this.homeTop
+
+  aboutUs = [
+    {
+      title: 'company',
+      route: ''
+    },
+      {
+      title: 'case studies',
+      route: ''
+    },
+    
+      {
+      title: 'investor relations',
+      route: ''
+    },
+  ]
 
 }
