1dba9187905da87019ca49b1d140c9ccf240a233
phillipnzau <nzauonline@gmail.com>
2023-07-12 10:25:52 +0300

    nav and request demo linking

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import Swiper from 'swiper';
 import { register } from 'swiper/element/bundle';
+import { RouterLink } from '@angular/router';
 // import Swiper and modules styles
 
 register();
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrls: ['./home.component.scss']
--- src/app/pages/solutions/solutions.component.ts
+++ src/app/pages/solutions/solutions.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-solutions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './solutions.component.html',
   styleUrls: ['./solutions.component.scss']
 })
